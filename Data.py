c050ad42f1b57211ce33e36540d6fd947102791b
Jtalaat <john.abdelmasseh@outlook.com>
2022-06-03 18:35:34 +0200

    add function

---
+++ Data.py
@@ -0,0 +1,15 @@
+MENU = {
+    "espresso": {"ingredients": {"water": 50,"coffee": 18,},"cost": 1.5,},
+    "latte": {"ingredients": {"water": 200,"milk": 150,"coffee": 24,},"cost": 2.5,},
+    "cappuccino": {"ingredients": {"water": 250,"milk": 100,"coffee": 24,},"cost": 3.0,}
+}
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+coins ={
+    "penny":1,
+    "Nickel":5,
+    "Dime":10,
+    "Quarter":25}
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-MENU = {
-    "espresso": {"ingredients": {"water": 50,"coffee": 18,},"cost": 1.5,},
-    "latte": {"ingredients": {"water": 200,"milk": 150,"coffee": 24,},"cost": 2.5,},
-    "cappuccino": {"ingredients": {"water": 250,"milk": 100,"coffee": 24,},"cost": 3.0,}
-}
-print(MENU["espresso"])
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+from Data import resources,MENU,coins
 
+def checkOrder(order,resour,coins):
+    print("")
+def checkResources():
+    print("")
+def checkCoins():
+    print("")
+def convertCoin(penny,Nicke,Dime,Quarter):
+    return ( (penny/100) + (Nicke/100) + (Dime/100) + (Quarter/100) )
+
+Responce = input("What would you like? (E ==> espresso L ==> latte C ==> cappuccino): ").lower()
+
+penny= int(input("how many penny: "))
+Nicke=int(input("how many Nicke: "))
+Dime=int(input("how many Dime: "))
+Quarter=int(input("how many Quarter: "))
